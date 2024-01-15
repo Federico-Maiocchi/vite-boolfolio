5d8a869ae81acaab5f32cb1fc06645ea19dd5e83
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-01-15 16:50:27 +0100

    ADD: styles ProjectCard

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,57 +2,82 @@
 
 
 export default {
-   props: {
+    props: {
         itemProject: {
-            type:Object,
+            type: Object,
 
-            required:true,
+            required: true,
         }
-   },
+    },
 
     data() {
         return {
-            
-           
-            
+
+
+
         };
     },
 
     methods: {
-        
+
     },
 
     created() {
-            
+
     },
-    
+
     mounted() {
-        
+
     }
 }
 
 </script>
 
 <template>
-      
-    <div class="card-project">
-        <h3 > {{ itemProject.title }}</h3>
-        <p>{{ itemProject.type.name }}</p>
-        <div class="row">
-            <p v-for="technology in itemProject.technologies">{{ technology.name }}</p>
+    <div class="col-4">
+        <div class="card-project">
+            <div class="card-head">
+                <h3 class="mb-2"><span class="text-bold">Titolo:</span> {{ itemProject.title }}</h3>
+                <p class="mb-2"><span class="text-bold">Slug:</span> {{ itemProject.slug }} </p>
+                <p class="mb-2"><span class="text-bold">Tipo:</span> {{ itemProject.type.name }}</p>
+            </div>
+            <div class="card-body ">
+                <p class="text-bold">Tecnologia: </p>
+                <div class="row mb-2">
+                    <p  class="col-3" v-for="technology in itemProject.technologies">{{ technology.name }}</p> 
+                </div>
+                <p class="mb-2"><span class="text-bold">Img:</span> {{ itemProject.img }}</p>
+                <p><span class="text-bold">Descrizione:</span> {{ itemProject.description }}</p>
+            </div>
+            
+            
+            
         </div>
-        
     </div>
-
 </template>
 
 <style lang="scss" scoped>
-
-.card-project {
+.text-bold {
+    font-weight: bold;
+}
+.col-4 {
     padding: 10px;
     
+    display: flex;
+} 
+.card-project {
+    padding: 20px;
+    border: 1px solid black;
+    flex:1;
+
 }
 
-    
+.card-head {
+    margin-bottom: 5px;
+}
+
+.card-body {
+    margin-bottom: 5px;
+}
 
 </style>
