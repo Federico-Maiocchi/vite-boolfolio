d286dc630a79db755add24d44a22014b9818188e
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-01-15 14:08:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script >
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>CIao mondo</h1>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+
+
+</style>
