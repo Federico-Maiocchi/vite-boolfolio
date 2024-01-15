3da6e0990f5052b4ccb6d00a810e37b2446e4066
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-01-15 15:03:04 +0100

    ADD: import axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <script >
-// import HelloWorld from './components/HelloWorld.vue'
+
+import ProjectIndex from './views/project/index.vue';
+
+
+export default {
+  components: {
+    ProjectIndex
+
+  },
+
+  data() {
+      return {
+          
+        
+          
+      };
+  },
+
+  created() {
+    
+  },
+
+  methods: {
+    
+  },
+
+
+
+  mounted() {
+      
+  }
+}
+
 </script>
 
 <template>
   <h1>CIao mondo</h1>
+  <ProjectIndex />
 </template>
 
 <style lang="scss">
---
+++ src/views/project/index.vue
@@ -0,0 +1,54 @@
+<script>
+import axios from 'axios';
+
+export default {
+    components: {
+    
+    
+    },
+
+    data() {
+        return {
+            projects:[],
+            BASE_URL: 'http://127.0.0.1:8000/api'
+           
+            
+        };
+    },
+
+    methods: {
+        fetchProjects() {
+                axios.get(`${this.BASE_URL}/projects`)
+                .then((res) => {
+                    console.log(res)
+                    this.projects = res.data.results
+                })
+            }
+    },
+
+    created() {
+            this.fetchProjects()
+    },
+    
+
+    mounted() {
+        
+    }
+}
+
+</script>
+
+<template>
+    <h2>Index.vue prova</h2>
+
+    <h1 v-for="project in projects" :key="project.id"> {{ project.title }}</h1>
+</template>
+
+<style lang="scss" scoped>
+
+
+
+
+    
+
+</style>
