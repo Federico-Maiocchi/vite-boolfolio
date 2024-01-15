7cef9894b676996512cf8a53ff18f666c053ee3d
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-01-15 15:31:05 +0100

    ADD: stamp axios in dom

--- src/views/project/index.vue
+++ src/views/project/index.vue
@@ -41,13 +41,40 @@ export default {
 <template>
     <h2>Index.vue prova</h2>
 
-    <h1 v-for="project in projects" :key="project.id"> {{ project.title }}</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card-project"></div>
+                <div v-for="project in projects" :key="project.id">
+                    <h3 > {{ project.title }}</h3>
+                    <p>{{ project.type.name }}</p>
+                    <p class="row" v-for="technology in project.technologies">{{ technology.name }}</p>
+                </div>
+            </div>    
+        </div>
+    </div>
+
+    
+
+    
+    
 </template>
 
 <style lang="scss" scoped>
 
+.container {
+    margin: 0 auto;
+    width: 900px;
+}
 
+.row {
+    display: flex;
+    flex-wrap: wrap;
+}
 
+.col-3 {
+    flex-basis: calc((100 / 12) * 3);
+}
 
     
 
